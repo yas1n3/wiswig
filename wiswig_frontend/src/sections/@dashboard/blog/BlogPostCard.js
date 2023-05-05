166bad0ea382d1748fab3a6e1eee37d22712fce0
yas1n3 <78967185+yas1n3@users.noreply.github.com>
2023-05-05 06:13:16 +0100

    Update BlogPostCard.js

--- wiswig_frontend/src/sections/@dashboard/blog/BlogPostCard.js
+++ wiswig_frontend/src/sections/@dashboard/blog/BlogPostCard.js
@@ -8,6 +8,9 @@ import useSWR, { mutate } from 'swr';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import { useSelector } from 'react-redux';
+
 import { NewslettersContext } from '../../../context/NewslettersContext';
 
 const StyledCardMedia = styled('div')({
@@ -58,31 +61,13 @@ export default function BlogPostCard({ newsletter, onNewsletterDelete, index, sl
   const newslettersContext = useContext(NewslettersContext);
   const { description, HTMLcontent, createdAt } = newsletter;
 
-
-
-  const [previewImage, setPreviewImage] = React.useState(null);
   const [newsletters, setNewsletters] = useState([]);
   const [templateJson, setTemplateJson] = useState({});
 
   const coverUrl = `/assets/images/covers/${newsletter.cover}.jpg`;
 
-
-
-
-
-
-  React.useEffect(() => {
-    if (HTMLcontent) {
-      htmlToImage
-        .toPng(HTMLcontent)
-        .then((dataUrl) => {
-          setPreviewImage(dataUrl);
-        })
-        .catch((error) => {
-          console.error('Error generating preview image', error);
-        });
-    }
-  }, [HTMLcontent]);
+  const { user } = useSelector((state) => state.user);
+  const { enqueueSnackbar } = useSnackbar();
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const handleClick = (event) => {
@@ -91,22 +76,18 @@ export default function BlogPostCard({ newsletter, onNewsletterDelete, index, sl
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const { data } = useSWR(`http://localhost:4000/newsletter/newsletter/${newsletter._id}`, async (url) => {
-    const response = await axios.get(url);
-    return response.data;
-  });
+
   const handleDelete = async () => {
+
     try {
       await axios.post(`http://localhost:4000/newsletter/delete/${newsletter._id}`);
       console.log(`Newsletter with id ${newsletter._id} has been deleted`);
-      mutate(`http://localhost:4000/newsletter/${newsletter._id}`, undefined, true);
-      // newslettersContext.deleteNewsletter(newsletter._id);
-      setNewsletters((prevNewsletters) =>
-        prevNewsletters.filter((prevNewsletter) => prevNewsletter._id !== newsletter._id)
-      );
       window.location.reload();
     } catch (error) {
+      enqueueSnackbar('You are not authorized to delete this newsletter', { variant: 'error' });
       console.error(`Error deleting newsletter with id ${newsletter._id}`, error);
+
+
     }
   };
 
@@ -122,13 +103,11 @@ export default function BlogPostCard({ newsletter, onNewsletterDelete, index, sl
   const navigate = useNavigate();
   const handleEdit = async () => {
     try {
-
       navigate(`/dashboard/editor/${newsletter._id}`, { state: { templateJson: newsletter.JSONcontent } });
     } catch (error) {
       console.error(`Error navigating to editor for newsletter with id ${newsletter._id}`, error);
     }
   };
-
 
   const handleSendTo = () => {
     console.log(`Sending newsletter with slug: ${slug} to...`);
@@ -136,7 +115,10 @@ export default function BlogPostCard({ newsletter, onNewsletterDelete, index, sl
 
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <Card sx={{ position: 'relative' }}>
+      <Card sx={{
+        position: 'relative',
+        backgroundColor: newsletter.JSONcontent ? 'inherit' : 'yellow',
+      }}>
         <CardContent>
           <StyledCardMedia>
             <StyledCover src={coverUrl} alt={newsletter.title} />
@@ -149,14 +131,18 @@ export default function BlogPostCard({ newsletter, onNewsletterDelete, index, sl
               <MoreVertIcon />
             </IconButton>
             <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-              <MenuItem
-                onClick={() => {
-                  handleClose();
-                  handleEdit();
-                }}
-              >
-                Edit
-              </MenuItem>
+              {newsletter.creator && user && newsletter.creator._id === user._id ? (
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    handleEdit();
+                  }}
+                >
+                  Edit
+                </MenuItem>
+              ) : (
+                <MenuItem disabled>Cannot Edit</MenuItem>
+              )}
               <MenuItem
                 onClick={() => {
                   handleClose();
@@ -173,25 +159,26 @@ export default function BlogPostCard({ newsletter, onNewsletterDelete, index, sl
               >
                 Duplicate
               </MenuItem>
-              <MenuItem
-                onClick={() => {
-                  handleClose();
-                  handleDelete();
-                }}
-              >
-                Delete
-              </MenuItem>
+              {newsletter.creator && user && newsletter.creator._id === user._id ? (
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    handleDelete();
+                  }}
+                >
+                  Delete
+                </MenuItem>) : (
+                <MenuItem disabled>Cannot Delete</MenuItem>
+              )}
             </Menu>
           </Box>
           <Typography variant="body2" color="text.secondary" gutterBottom>
             {description}
           </Typography>
           <StyledInfo>
-
             <Typography variant="body2" color="inherit">
               {new Date(createdAt).toLocaleDateString()}
             </Typography>
-
           </StyledInfo>
         </CardContent>
       </Card>
